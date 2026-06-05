a95a9f2766f1d3112c50587c5b78d4ceca8284ed
Yong Joon Lee <112607019+infected4098@users.noreply.github.com>
2026-06-05 14:57:54 +0900

    Update semambapp.py

--- model/semambapp.py
+++ model/semambapp.py
@@ -8,6 +8,33 @@ from functools import partial
 from .frequencyglp import FrequencyGLP
 from .FAN import FANFFN, FANFFN_gate_freq, FANFFN_gate_channel
 from .encdec import DenseEncoder, MagDecoder, PhaseDecoder
+from einops import rearrange
+
+class LayerNorm(nn.Module):
+    """ LayerNorm that supports two data formats: channels_last (default) or channels_first. 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+    """
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError 
+        self.normalized_shape = (normalized_shape, )
+    
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
 
 
 def compute_auto_padding_2d(stride_t=2, stride_f=2, kernel_t=3, kernel_f=3):
